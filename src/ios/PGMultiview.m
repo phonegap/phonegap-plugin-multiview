f3afcf0e2879b19ad8afcccf4d00f437a212f28e
Jesse MacFadyen <purplecabbage@gmail.com>
2016-02-28 01:00:54 -0800

    added nav-controller delegate to change visibility of navbar when views are pushed+popped

--- src/ios/PGMultiview.m
+++ src/ios/PGMultiview.m
@@ -7,6 +7,9 @@
 #import <Cordova/CDV.h>
 #import "PGMultiview.h"
 
+
+#pragma mark PGMultiviewController - CDVViewController subclass
+
 @implementation PGMultiviewController
 
 - (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
@@ -27,6 +30,31 @@
     return YES;
 }
 
+// TODO: override this, and use the config.xml for the currently loaded page
+// also, add ALL orientations to the application
+// - (BOOL)supportsOrientation:(UIInterfaceOrientation)orientation
+// {
+//     return [self.supportedOrientations containsObject:[NSNumber numberWithInt:orientation]];
+// }
+
+#pragma mark UINavigationControllerDelegate
+
+- (void)navigationController:(UINavigationController *)navigationController
+      willShowViewController:(UIViewController *)viewController
+                    animated:(BOOL)animated
+{
+    if(viewController == self.viewController)
+    {
+        navigationController.navigationBarHidden = YES;
+    }
+    else
+    {
+        navigationController.navigationBarHidden = NO;
+    }
+}
+
+
+#pragma mark JS Callable Plugin API
 
 - (void)dismissView:(CDVInvokedUrlCommand*)command
 {
@@ -40,17 +68,20 @@
     childViewController = [[PGMultiviewController alloc] init];
     childViewController.startPage = [command argumentAtIndex:0];
     
+    // TODO: set proper config.xml -> childViewController.configFile
+    
     if(self.viewController.navigationController == NULL)
     {
         UINavigationController* nav = [[UINavigationController alloc] init];
         nav.navigationBarHidden = NO;
+        nav.delegate = self;
         self.webView.window.rootViewController = nav;
         [nav pushViewController:self.viewController animated:NO];
         nav.hidesBarsOnSwipe  = YES;
         nav.hidesBarsOnTap = YES;
-
+        
     }
-
+    
     [self.viewController.navigationController pushViewController:childViewController animated:YES];
 }
 
