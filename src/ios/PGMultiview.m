9c105ab1c95f9b166e6d7c11d541f2ee806408fe
Jesse MacFadyen <purplecabbage@gmail.com>
2016-02-27 23:11:01 -0800

    Subclassed CDVViewController to add shake to dismiss. Implement shouldOverrideLoadWithRequest, just returns YES currently.

--- src/ios/PGMultiview.m
+++ src/ios/PGMultiview.m
@@ -7,32 +7,50 @@
 #import <Cordova/CDV.h>
 #import "PGMultiview.h"
 
+@implementation PGMultiviewController
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (UIEventSubtypeMotionShake) {
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+}
+
+@end
+
 
 @implementation PGMultiview
 
 @synthesize childViewController;
 
+- (BOOL) shouldOverrideLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    return YES;
+}
+
 
 - (void)dismissView:(CDVInvokedUrlCommand*)command
 {
     [self.viewController.navigationController popViewControllerAnimated:YES];
+    self.viewController.navigationController.navigationBarHidden = YES;
     childViewController = NULL;
-
 }
 
 - (void)loadView:(CDVInvokedUrlCommand*)command
 {
-    childViewController = [[CDVViewController alloc] init];
+    childViewController = [[PGMultiviewController alloc] init];
     childViewController.startPage = [command argumentAtIndex:0];
     
     if(self.viewController.navigationController == NULL)
     {
         UINavigationController* nav = [[UINavigationController alloc] init];
-        nav.navigationBarHidden = YES;
+        nav.navigationBarHidden = NO;
         self.webView.window.rootViewController = nav;
         [nav pushViewController:self.viewController animated:NO];
+        nav.hidesBarsOnSwipe  = YES;
+        nav.hidesBarsOnTap = YES;
+
     }
-    
+
     [self.viewController.navigationController pushViewController:childViewController animated:YES];
 }
 
