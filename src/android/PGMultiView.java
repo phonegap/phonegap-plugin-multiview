90e1ee1f238a4c172e8cca478e9341a3c262f9de
Jesse MacFadyen <purplecabbage@gmail.com>
2016-07-22 23:42:43 -0700

    some basic android - far from complete

--- src/android/PGMultiView.java
+++ src/android/PGMultiView.java
@@ -1,11 +1,12 @@
 
-package phonegap.PGMultiView;
+package phonegap.pgmultiview;
 
 import android.annotation.SuppressLint;
 import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.Config;
+import org.apache.cordova.CordovaActivity;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaHttpAuthHandler;
 import org.apache.cordova.CordovaPlugin;
@@ -16,10 +17,15 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.Intent;
+
+
 @SuppressLint("SetJavaScriptEnabled")
 public class PGMultiView extends CordovaPlugin {
 
     protected static final String LOG_TAG = "PGMultiView";
+
+    protected Intent intent;
 
     /**
      * Executes the request and returns PluginResult.
@@ -27,26 +33,32 @@ public class PGMultiView extends CordovaPlugin {
      * @param action the action to execute.
      * @param args JSONArry of arguments for the plugin.
      * @param callbackContext the callbackContext used when calling back into JavaScript.
-     * @return A PluginResult object with a status and message.
+     * @return boolean success or fail
      */
     public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("loadView")) {
             final String url = args.getString(0);
 
             Log.d(LOG_TAG, "url = " + url);
-            // PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, result);
-            // pluginResult.setKeepCallback(true);
-            // callbackContext.sendPluginResult(pluginResult);
+
+            intent = new Intent(this.cordova.getActivity(), com.phonegap.PGMultiViewdemo.MainActivity.class);
+            // todo: figure out how to pass this in
+            //intent.putExtra("startUrl", "http://www.phonegap.com");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            this.cordova.getActivity().startActivity(intent);
+
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+            pluginResult.setKeepCallback(true);
+            callbackContext.sendPluginResult(pluginResult);
+
 
         }
         else if (action.equals("dismissView")) {
-            //closeDialog();
+            // todo: more to it than this
+            this.cordova.getActivity().finish();
         }
-
         return true;
-
     }
-
-
 
 }
