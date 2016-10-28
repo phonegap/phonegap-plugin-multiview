0e6d0a6fede495ecc0367902f69a9a180ac15f68
Jesse MacFadyen <purplecabbage@gmail.com>
2016-10-28 10:45:39 -0700

    clean up in prep for better demo

--- demo/www/js/index.js
+++ demo/www/js/index.js
@@ -1,55 +1,22 @@
 
-var currentDepth = 1;
+
+document.addEventListener("DOMContentLoaded",function() {
 
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
+});
 
-        currentDepth = localStorage.getItem("currentDepth") || 1;
+document.addEventListener("deviceready",function() {
 
-        localStorage.setItem("currentDepth",currentDepth);
+    btnShowNext.addEventListener("click",function(){
+        alert("showing it");
+        PGMultiView.loadView("index2.html");
+    });
 
-        hdrDepth.innerText = hdrDepth.innerText +  " : " + currentDepth;
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    // btnDismiss.addEventListener("click",function(){
+    //     localStorage.setItem("currentDepth",--currentDepth);
+    //     PGMultiView.dismissView();
+    // });
+});
 
-        app.receivedEvent('deviceready');
 
-        btnShowNext.addEventListener("touchend",function(){
-            localStorage.setItem("currentDepth",++currentDepth);
-            PGMultiView.loadView("www/index.html");
-        });
 
-        btnDismiss.addEventListener("touchend",function(){
-            localStorage.setItem("currentDepth",--currentDepth);
-            PGMultiView.dismissView();
-        });
-
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
