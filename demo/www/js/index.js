380d263bb111de96966c659f0260eaa78ddc70a8
Jesse MacFadyen <purplecabbage@gmail.com>
2016-10-28 17:39:19 -0700

    Cleaned up demo, just what it needs to be for now

--- demo/www/js/index.js
+++ demo/www/js/index.js
@@ -1,22 +1,13 @@
 
 
 document.addEventListener("DOMContentLoaded",function() {
-
-
 });
 
 document.addEventListener("deviceready",function() {
 
     btnShowNext.addEventListener("click",function(){
-        alert("showing it");
-        PGMultiView.loadView("index2.html");
+        var txt = txtArea.value;
+        PGMultiView.loadView("index2.html",txt);
     });
-
-    // btnDismiss.addEventListener("click",function(){
-    //     localStorage.setItem("currentDepth",--currentDepth);
-    //     PGMultiView.dismissView();
-    // });
 });
-
-
 
