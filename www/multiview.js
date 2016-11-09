82b3dcf9336cc2fc7c1d46cdd4d4c47773d518c9
Jesse MacFadyen <purplecabbage@gmail.com>
2016-11-09 11:36:49 -0800

    Fixed js - reverting changes, added Sterling as contributor

--- www/multiview.js
+++ www/multiview.js
@@ -1,16 +1,14 @@
-// (c)2016 Jesse MacFadyen and Sterling Gerritz,  Adobe Systems Incorporated
-cordova.define("phonegap-plugin-multiview.multiview", function(require, exports, module) {
+// (c)2016 Jesse MacFadyen,  Adobe Systems Incorporated
+
 var exec = require('cordova/exec');
 
 module.exports = {
 
-  loadView:function(strPath) {
-    exec(null,null,"PGMultiView","loadActivity",[strPath]);
-  },
+    loadView:function(strPath) {
+        exec(null,null,"PGMultiView","loadActivity",[strPath]);
+    },
 
     dismissView:function() {
         exec(null,null,"PGMultiView","dismissActivity",[]);
     }
 }
-
-});
