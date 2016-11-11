5ed8bd048e74db9ee340b0187532a346b3296dae
Sterling <sterlingann@gmail.com>
2016-11-10 18:26:18 -0800

    resolved passed URL Issue #15
    removed console logs and updated name of actions in js to view
    
    updated for past error
    
    utilized cordova urlutil to make url string an absolute path

--- www/multiview.js
+++ www/multiview.js
@@ -1,14 +1,19 @@
+
 // (c)2016 Jesse MacFadyen,  Adobe Systems Incorporated
 
 var exec = require('cordova/exec');
-
+var urlutil = require('cordova/urlutil');
 module.exports = {
-
-    loadView:function(strPath) {
-        exec(null,null,"PGMultiView","loadActivity",[strPath]);
-    },
-
+loadView:function(strPath) {
+strPath = urlutil.makeAbsolute(strPath);
+window.alert("strPath = " + strPath);
+      exec(null,null,"PGMultiView","loadView",[strPath]);
+  },
     dismissView:function() {
-        exec(null,null,"PGMultiView","dismissActivity",[]);
+        exec(null,null,"PGMultiView","dismissView",[]);
     }
 }
+
+
+
+
