db983e3d37d5c2adbbc5d2f224395e34b07f4d5a
Sterling <sterlingann@gmail.com>
2016-11-10 17:55:00 -0800

    resolved issue with passed URL

--- src/android/PGMultiView.java
+++ src/android/PGMultiView.java
@@ -1,29 +1,25 @@
+package phonegap.pgmultiview;
 
-        package org.apache.cordova.pgmultiview;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LOG;
+import org.apache.cordova.Config;
+import org.apache.cordova.CordovaActivity;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaHttpAuthHandler;
+import org.apache.cordova.PluginManager;
+import org.apache.cordova.PluginResult;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-        import org.apache.cordova.CordovaWebView;
-        import org.apache.cordova.CallbackContext;
-        import org.apache.cordova.CordovaPlugin;
-        import org.apache.cordova.CordovaInterface;
-        import org.apache.cordova.CallbackContext;
-        import org.apache.cordova.CordovaPlugin;
-        import org.apache.cordova.LOG;
-        import org.apache.cordova.Config;
-        import org.apache.cordova.CordovaActivity;
-        import org.apache.cordova.CordovaArgs;
-        import org.apache.cordova.CordovaHttpAuthHandler;
-        import org.apache.cordova.PluginManager;
-        import org.apache.cordova.PluginResult;
-        import org.json.JSONException;
-        import org.json.JSONObject;
-
-       // import android.manifest;
-        import android.app.Activity;
-        import android.os.Build;
-        import android.content.Intent;
-
-
-
+// import android.manifest;
+import android.app.Activity;
+import android.os.Build;
+import android.content.Intent;
 
 public class PGMultiView extends CordovaPlugin {
 
@@ -31,35 +27,30 @@ public class PGMultiView extends CordovaPlugin {
 
     protected Intent intent;
 
-
 // @param action the action to execute.
 // @param args JSONArry of arguments for the plugin.
 // @param the context used when calling back to JS .
 // @return boolean success or fail
 
 //cordova is context of main activity.  webView is the webview we are running the main Activity (cordova) inside of.
-
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
     }
 
     public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
-
-        if (action.equals("loadActivity")) {
+        if (action.equals("loadView")) {
             final String url = args.getString(0);
-
             LOG.d(LOG_TAG, "url = " + url);
             startCordovaActivity(url);
-
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+            pluginResult.setKeepCallback(true);
+            callbackContext.sendPluginResult(pluginResult);
         }
-
-        else if (action.equals("dismissActivity")) {
-
+        else if (action.equals("dismissView")) {
             quit();
         }
         return true;
     }
-
 
     private void startCordovaActivity(final String url){ //url is passed in from execute, its args.getstring(0)
         Intent intent = new Intent(this.cordova.getActivity(), PGMultiViewActivity.class);
@@ -69,10 +60,9 @@ public class PGMultiView extends CordovaPlugin {
     }
 
     private void quit(){
-
         LOG.d(LOG_TAG, "Closing the secondary View/Activity");
-
         this.cordova.getActivity().finish();
     }
 
 }
+
--- www/multiview.js
+++ www/multiview.js
@@ -1,14 +1,30 @@
 // (c)2016 Jesse MacFadyen,  Adobe Systems Incorporated
-
+cordova.define("phonegap-plugin-multiview.multiview", function(require, exports, module) {
 var exec = require('cordova/exec');
 
 module.exports = {
 
-    loadView:function(strPath) {
-        exec(null,null,"PGMultiView","loadActivity",[strPath]);
-    },
+  loadView:function(strPath) {
+      var newUrl = strPath;
+      if ( strPath.indexOf(":")  == -1 ) { //if its a url with a colon, so just load it
+            var currentLocation = window.location.href;
+            var lastSlash = currentLocation.lastIndexOf("/");
+            console.log(currentLocation)
+            console.log(lastSlash)
+            console.log(strPath)
+            console.log(currentLocation.substr(0,lastSlash))
+            console.log('hi');
+            newUrl = currentLocation.substr(0,lastSlash) + "/" + strPath;
+            console.log('newURL: ' + newUrl);
+      }
+
+      exec(null,null,"PGMultiView","loadView",[newUrl]);
+  },
 
     dismissView:function() {
-        exec(null,null,"PGMultiView","dismissActivity",[]);
+        exec(null,null,"PGMultiView","dismissView",[]);
     }
 }
+
+});
+
