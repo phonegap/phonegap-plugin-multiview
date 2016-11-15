bcabb9eee07a67a07ace3ed0beb18a38aa4708cc
Sterling <sterlingann@gmail.com>
2016-11-14 18:30:24 -0800

    fixed error in merge conflict

--- www/multiview.js
+++ www/multiview.js
@@ -1,10 +1,7 @@
-
 // (c)2016 Jesse MacFadyen,  Adobe Systems Incorporated
-
 var exec = require('cordova/exec');
 var urlutil = require('cordova/urlutil');
 module.exports = {
-<<<<<<< HEAD
 loadView:function(strPath) {
 strPath = urlutil.makeAbsolute(strPath);
 window.alert("strPath = " + strPath);
