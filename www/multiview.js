57b147ba606b33b3a6c096af8a064c8235c6ecc4
Jesse MacFadyen <purplecabbage@gmail.com>
2016-11-15 17:49:07 -0800

    resolve url to absolute using cordova js util method

--- www/multiview.js
+++ www/multiview.js
@@ -1,10 +1,13 @@
 // (c)2016 Jesse MacFadyen,  Adobe Systems Incorporated
 
 var exec = require('cordova/exec');
+var urlutil = require('cordova/urlutil');
 
 module.exports = {
 
     loadView:function(strPath) {
+        // make sure it is absolute using cordova.js util method.
+        strPath = urlutil.makeAbsolute(strPath);
         exec(null,null,"PGMultiView","loadActivity",[strPath]);
     },
 
