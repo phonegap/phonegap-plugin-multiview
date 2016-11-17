3e455deeb840f1a911c20f10cc0c650d8a9c3f12
Sterling <sterlingann@gmail.com>
2016-11-17 12:10:58 -0800

    cleaned up passing data branch

--- src/android/PGMultiViewActivity.java
+++ src/android/PGMultiViewActivity.java
@@ -1,31 +1,24 @@
 package phonegap.pgmultiview;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.app.Activity;
-import android.app.Dialog;
-import android.widget.TextView;
-import android.widget.RelativeLayout;
-import android.widget.ProgressBar;
-import android.widget.LinearLayout;
-import android.view.ViewGroup;
 import org.apache.cordova.CordovaActivity;
-import android.content.DialogInterface;
-import android.view.KeyEvent;
 
 public class PGMultiViewActivity extends CordovaActivity {
 
-    Activity secondActivity;
+    public String message;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        secondActivity=this;
 
-        Bundle bundle =getIntent().getExtras();
+        Bundle bundle = getIntent().getExtras();
         String url = bundle.getString("start URL");
-        loadUrl(url);
+        message = bundle.getString("Message to child");
+        loadUrl("file:///android_asset/www/" + url);
     }
 
+    public String getMessage() {
+        return message;
+    }
 }
 
