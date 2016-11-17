f48aee891d9ce29144c9880dd17dd60062ec6588
Jesse MacFadyen <purplecabbage@gmail.com>
2016-11-16 16:13:31 -0800

    added jshinting, and corrected the things it told me to

--- src/windows/NotifyExec.js
+++ src/windows/NotifyExec.js
@@ -6,14 +6,14 @@ var href = window.location.href;
 var oldGet = execProxy.get;
 
 // if we are loaded from app-data we need to shim in a new bridge
-if (href.indexOf("ms-appdata") == 0) {
+if (href.indexOf("ms-appdata") === 0) {
     execProxy.get = function (service, action) {
         // we will only replace apis that exist
         if(oldGet(service,action)) {
             return function (onSuccess, onError, args) {
                 var obj = { service: service, action: action, args: args };
                 window.external.notify("gap:" + JSON.stringify(obj));
-            }
+            };
         }
     };
 }
