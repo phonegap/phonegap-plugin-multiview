1c9f24a663654ff104cd888e9ed351894a2b6081
Jesse MacFadyen <purplecabbage@gmail.com>
2016-12-09 15:27:20 -0800

    added asset-test to actually load another page and test it. (#46)

--- tests/tests.js
+++ tests/tests.js
@@ -15,5 +15,18 @@ exports.defineAutoTests = function() {
             expect(PGMultiView.dismissView).toBeDefined();
             expect(PGMultiView.dismissView).toEqual(jasmine.any(Function));
         });
+
+        it("should navigate to a child view", function() {
+            var errHandler = function (err) {
+                console.log("error " + err);
+                expect("errorHandlerCalled").toEqual(false);
+            };
+            var testMessage = "hey, can this make it there and back again?";
+            PGMultiView.loadView("fixtures/index2.html", testMessage, function(res) {
+                console.log("success " + res);
+                expect(res).toEqual(testMessage);
+            },errHandler
+            );
+        });
     });
 };
