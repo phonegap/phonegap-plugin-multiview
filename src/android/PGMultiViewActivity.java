9d40361ba7a64baad46a85749edd4274401314cd
Sterling <sterlingann@gmail.com>
2016-12-09 11:56:37 -0800

    made backbutton behavior global across plugin

--- src/android/PGMultiViewActivity.java
+++ src/android/PGMultiViewActivity.java
@@ -2,6 +2,8 @@ package phonegap.pgmultiview;
 
         import android.os.Bundle;
         import org.apache.cordova.CordovaActivity;
+        import android.content.Intent;
+        import android.app.Activity;
 
 public class PGMultiViewActivity extends CordovaActivity {
 
@@ -15,6 +17,18 @@ public class PGMultiViewActivity extends CordovaActivity {
         message = bundle.getString("Message to child");
         loadUrl(url);
     }
+
+    @Override
+    public void onBackPressed() {
+        if(appView.canGoBack()){
+            appView.backHistory();
+        }
+        else if(appView.canGoBack()==false) {
+            Intent result = new Intent();
+            result.putExtra("Message to parent", "");
+            this.setResult(Activity.RESULT_OK, result);
+            this.finish();
+        }}
 
     public String getMessage() {
         return message;
