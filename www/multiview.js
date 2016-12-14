125555b287d757b121c8af58657a4c8e0b86299a
Sterling <sterlingann@gmail.com>
2016-12-13 17:45:55 -0800

    refactored for improved readability

--- www/multiview.js
+++ www/multiview.js
@@ -1,9 +1,9 @@
-/*global previousResult:true */
+
 var exec = require('cordova/exec');
 var urlutil = require('cordova/urlutil');
 var channel = require('cordova/channel');
 
-module.exports = {
+var alias = module.exports = {
     previousResult:null,
     getMessage:function(success, error) {
         exec(success,error,"PGMultiView","getMessage",[]);
@@ -16,10 +16,11 @@ module.exports = {
         exec(null,null,"PGMultiView","dismissView",[message]);
    } 
 };
+
 channel.onCordovaReady.subscribe(function() {
-    module.exports.getMessage( function(res){
-        previousResult = res;
+    alias.getMessage( function(res){
+        alias.previousResult = res;
     }, function(err) {
-   //Either parent is being loaded for the first time or there is no previous result
+        //Either parent is being loaded for the first time or there is no previous result
     });
 });
