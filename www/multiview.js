044983e2ff83666bbaf105149fbee280118f6b1a
Sterling Ann Gerritz <sterlingann@gmail.com>
2016-12-13 20:56:55 -0800

    added check for previous result to MV.js (#48)
    * added check for previous result to MV.js
    
    * refactored for improved readability

--- www/multiview.js
+++ www/multiview.js
@@ -1,16 +1,26 @@
 
 var exec = require('cordova/exec');
 var urlutil = require('cordova/urlutil');
+var channel = require('cordova/channel');
 
-module.exports = {
+var alias = module.exports = {
+    previousResult:null,
+    getMessage:function(success, error) {
+        exec(success,error,"PGMultiView","getMessage",[]);
+    },
     loadView:function(strUrl, message, success, error) {
         strUrl = urlutil.makeAbsolute(strUrl);
         exec(success, error,"PGMultiView","loadView",[strUrl, message]);
     },
     dismissView:function(message) {
         exec(null,null,"PGMultiView","dismissView",[message]);
-    },
-    getMessage:function(success, error) {
-        exec(success,error,"PGMultiView","getMessage",[]);
-    }
+   } 
 };
+
+channel.onCordovaReady.subscribe(function() {
+    alias.getMessage( function(res){
+        alias.previousResult = res;
+    }, function(err) {
+        //Either parent is being loaded for the first time or there is no previous result
+    });
+});
